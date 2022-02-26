ac79bf61a42b557751bcbdff2b89991e9d266c43
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-02-26 19:16:08 -0300

    fix: update route contract

--- packages/server/src/contracts/http.ts
+++ packages/server/src/contracts/http.ts
@@ -14,7 +14,7 @@ export type Middleware<T, U> = (
 export type Route<T, U> = {
   route: string;
   method: Method;
-  handle: (request: T) => U;
+  handle: (request: T, response: U) => U;
   middlewares?: Middleware<T, U>[];
 };
 

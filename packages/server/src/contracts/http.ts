97729c379c7cf079b21cf597221ca50961f8c0d8
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-02-26 18:46:54 -0300

    chore: update http contracts

--- packages/server/src/contracts/http.ts
+++ packages/server/src/contracts/http.ts
@@ -1,14 +1,21 @@
 export enum Method {
-  get,
-  post,
-  put,
-  delete,
+  get = "get",
+  post = "post",
+  put = "put",
+  delete = "delete",
 }
+
+export type Middleware<T, U> = (
+  request: T,
+  response: U,
+  next: Function,
+) => void;
 
 export type Route<T, U> = {
   route: string;
   method: Method;
   handle: (request: T) => U;
+  middlewares?: Middleware<T, U>[];
 };
 
 export abstract class Service<T> {
@@ -20,4 +27,14 @@ export abstract class Service<T> {
 
   public abstract listen(port: number): void;
   public abstract close(): void;
+}
+
+export abstract class Router<A, T, U> {
+  protected readonly routes: Route<T, U>[];
+
+  public constructor(routes: Route<T, U>[]) {
+    this.routes = routes;
+  }
+
+  public abstract create(): A;
 }

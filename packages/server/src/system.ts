cf0171e2922015ae55acabec5774e26c8ebe546b
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-05 13:18:48 -0300

    refactor: use contracts

--- packages/server/src/system.ts
+++ packages/server/src/system.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import * as E from "express";
 
 import { createSystem } from "~/components/implementation/system";
 import { Http } from "~/components/http";
@@ -6,9 +6,9 @@ import { Http } from "~/components/http";
 import * as HttpContracts from "~/contracts/http";
 import * as HttpAdapters from "~/adapters/http";
 
-const httpApplication = express();
+const express = E.default();
 
-const router = new HttpAdapters.HttpRouter([
+const router: HttpContracts.Router<E.Router, E.Request, E.Response> = new HttpAdapters.HttpRouter([
   {
     path: "/",
     method: HttpContracts.Method.get,
@@ -40,16 +40,16 @@ const router = new HttpAdapters.HttpRouter([
   },
 ]);
 
-httpApplication.use(express.json());
+express.use(E.json());
 
-httpApplication.use((_req, _res, next) => {
+express.use((_req, _res, next) => {
   console.log("Common middleware", Date.now());
   next();
 });
 
-httpApplication.use(router.create());
+express.use(router.create());
 
-const httpService = new HttpAdapters.HttpService(httpApplication);
+const httpService: HttpContracts.Service<E.Application> = new HttpAdapters.HttpService(express);
 
 export const system = createSystem({
   http: new Http(httpService, 3333),

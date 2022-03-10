512d9a9feebfbdb24942bb1e7a10c23ec0186bc5
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-10 20:28:05 -0300

    chore: update password handler contract

--- packages/server/src/contracts/password-handler.ts
+++ packages/server/src/contracts/password-handler.ts
@@ -1,6 +1,20 @@
 import { Password } from "~/domain/entities/values";
 
-export interface PasswordHandler {
-  encode(value: string): Promise<Password>;
-  compare(value1: string, value2: string): Promise<boolean>;
+export abstract class PasswordHandler {
+  protected readonly hashSalt: string;
+
+  public constructor(hashSalt: string) {
+    this.hashSalt = hashSalt;
+  }
+
+  public isValid(value: string): boolean {
+    if (!value || value.length < 8) {
+      return false;
+    }
+
+    return true;
+  }
+
+  abstract encode(value: string): Password;
+  abstract compare(value1: string, value2: string): boolean;
 }

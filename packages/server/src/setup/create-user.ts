ad217eb155142c2ba9c8f7133f2a1eb42563a591
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-10 21:13:16 -0300

    chore: add createuser setup

---
+++ packages/server/src/setup/create-user.ts
@@ -0,0 +1,37 @@
+import {
+  EmailValidator,
+  Method,
+  PasswordHandler,
+  PhoneValidator,
+  UsersRepo,
+} from "~/contracts";
+import { CreateUserController } from "~/domain/controllers";
+import { IdFactory } from "~/domain/factories/id-factory";
+import { UserFactory } from "~/domain/factories/user-factory";
+import { CreateUserUseCase } from "~/domain/usecases";
+
+const idFactory: IdFactory = {} as IdFactory;
+const emailValidator: EmailValidator = {} as EmailValidator;
+const passwordHandler: PasswordHandler = {} as PasswordHandler;
+const phoneValidator: PhoneValidator = {} as PhoneValidator;
+
+const userFactory = new UserFactory(
+  idFactory,
+  emailValidator,
+  passwordHandler,
+  phoneValidator,
+);
+
+const usersRepo: UsersRepo = {} as UsersRepo;
+
+const createUserUseCase = new CreateUserUseCase(userFactory, usersRepo);
+
+const createUserController = new CreateUserController(createUserUseCase);
+
+export function setupCreateUsers() {
+  return {
+    controller: createUserController,
+    route: "/users",
+    method: Method.post,
+  };
+}

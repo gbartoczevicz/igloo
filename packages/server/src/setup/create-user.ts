af0483fb5650ae399cd0e16dd2393b795ad79a60
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-17 19:38:05 -0300

    chore: add middleware to validate and inject dto on request

--- packages/server/src/setup/create-user.ts
+++ packages/server/src/setup/create-user.ts
@@ -6,6 +6,7 @@ import { UsersRepo } from "~/contracts/repositories";
 import { CreateUserController } from "~/domain/controllers";
 import { UserFactory } from "~/domain/factories";
 import { CreateUserUseCase } from "~/domain/usecases";
+import { CreateUserIn } from "~/dtos";
 
 const idProvider: IdProvider = new NodeIdProvider();
 
@@ -22,11 +23,24 @@ export function setupCreateUsers() {
     "/users",
     Method.post,
     (req, res, next) => {
-      controller.execute((req as any).user).then((result) =>
+      controller.execute((req as any).createUserIn).then((result) =>
         res.status(result.status).json(result.content)
       );
 
       next();
+    },
+    (req, res, next) => {
+      const result = CreateUserIn.create(req.body);
+
+      if (result instanceof CreateUserIn) {
+        (req as any).createUserIn = result;
+
+        return next();
+      }
+
+      res.status(result.status).json(
+        result.content.map((c) => ({ [c.field]: c.message })),
+      );
     },
   );
 }

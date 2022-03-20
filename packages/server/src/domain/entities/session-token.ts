9e99bff55e29337375bc68b2e4a0346a6f14094c
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-20 20:58:06 -0300

    chore: add new field

--- packages/server/src/domain/entities/session-token.ts
+++ packages/server/src/domain/entities/session-token.ts
@@ -1,11 +1,14 @@
 import { Id } from "./values";
 
 export class SessionToken {
+  public readonly id: Id;
+
   public readonly token: string;
 
   public readonly userId: Id;
 
-  public constructor(token: string, userId: Id) {
+  public constructor(id: Id, token: string, userId: Id) {
+    this.id = id;
     this.token = token;
     this.userId = userId;
   }

93945e5898f3f864ebb77f7e81afb032415f3775
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-21 19:22:26 -0300

    chore: add out session dto

--- packages/server/src/dtos/index.ts
+++ packages/server/src/dtos/index.ts
@@ -2,3 +2,4 @@ export * from "./create-user-in";
 export * from "./create-user-out";
 export * from "./common-error-out";
 export * from "./create-session-in";
+export * from "./create-session-out";

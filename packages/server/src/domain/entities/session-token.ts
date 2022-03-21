3292a0afedd5232e102d2118beaab4925bff0d5d
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-21 19:16:58 -0300

    chore: remove field from session token entity

--- packages/server/src/domain/entities/session-token.ts
+++ packages/server/src/domain/entities/session-token.ts
@@ -1,14 +1,11 @@
 import { Id } from "./values";
 
 export class SessionToken {
-  public readonly id: Id;
-
   public readonly token: string;
 
   public readonly userId: Id;
 
-  public constructor(id: Id, token: string, userId: Id) {
-    this.id = id;
+  public constructor(token: string, userId: Id) {
     this.token = token;
     this.userId = userId;
   }

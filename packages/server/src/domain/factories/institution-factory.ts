8c606280cddec77752b746e8d8ea732a9b0e0961
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-23 19:41:11 -0300

    chore: use dto at factory

--- packages/server/src/domain/factories/institution-factory.ts
+++ packages/server/src/domain/factories/institution-factory.ts
@@ -1,9 +1,11 @@
 import { Factory } from "~/contracts/domain";
+import { CreateInstitutionIn } from "~/dtos";
 import { Institution } from "../entities";
 import { IdFactory } from "./id-factory";
 import { PhoneFactory } from "./phone-factory";
 
-export class InstitutionFactory implements Factory<unknown, Institution> {
+export class InstitutionFactory
+  implements Factory<CreateInstitutionIn, Institution> {
   private readonly idFactory: IdFactory;
 
   private readonly phoneFactory: PhoneFactory;
@@ -13,11 +15,12 @@ export class InstitutionFactory implements Factory<unknown, Institution> {
     this.phoneFactory = phoneFactory;
   }
 
-  create(incoming: unknown): Institution {
-    const { name, uniqueId, phone } = incoming as any || {};
+  create(incoming: CreateInstitutionIn): Institution {
+    const { name, uniqueId } = incoming;
 
     const id = this.idFactory.create();
+    const phone = this.phoneFactory.create(incoming.phone);
 
-    return new Institution(id, name, uniqueId, this.phoneFactory.create(phone));
+    return new Institution(id, name, uniqueId, phone);
   }
 }

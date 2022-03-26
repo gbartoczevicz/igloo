4dbf89c90d755543f683e570c3772121b574f8ca
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-26 18:03:11 -0300

    chore: add new route

---
+++ packages/server/src/setup/routes/create-institution.ts
@@ -0,0 +1,18 @@
+import * as E from "express";
+import { HttpRoute } from "~/adapters/http";
+
+import { HttpStatus, Method, Middleware } from "~/contracts/http";
+import { SystemSetup } from "~/contracts/setup/system";
+
+type M = Middleware<E.Request, E.Response, E.NextFunction>;
+
+export function setupCreateInstitutions(
+  systemSetup: SystemSetup,
+  userAuthenticated: M,
+) {
+  return new HttpRoute("/institutions", Method.post, (req, res, next) => {
+    console.log("Institutions Route");
+
+    return res.sendStatus(HttpStatus.created);
+  }, userAuthenticated);
+}

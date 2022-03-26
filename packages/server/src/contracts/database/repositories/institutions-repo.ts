856037200cca5a5ba42b05901e597d49019a5126
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-25 22:58:08 -0300

    chore: add institution repo method

--- packages/server/src/contracts/database/repositories/institutions-repo.ts
+++ packages/server/src/contracts/database/repositories/institutions-repo.ts
@@ -1,8 +1,9 @@
 import { Institution } from "~/domain/entities";
-import { Cnpj, Phone } from "~/domain/entities/values";
+import { Cnpj, Id, Phone } from "~/domain/entities/values";
 
 export interface InstitutionsRepo {
   save(institution: Institution): Promise<void>;
+  findById(id: Id): Promise<Institution | null>;
   findByPhone(phone: Phone): Promise<Institution | null>;
   findByCnpj(cnpj: Cnpj): Promise<Institution | null>;
 }

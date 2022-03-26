74320f2d0bc28493fe27b9cec4ee34d0b9dbe914
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-26 18:01:24 -0300

    chore: add user into request

--- packages/server/src/@types/express.d.ts
+++ packages/server/src/@types/express.d.ts
@@ -1,8 +1,10 @@
+import { User } from "~/domain/entities";
 import { CreateSessionIn, CreateUserIn } from "~/dtos";
 
 declare global {
   namespace Express {
     interface Request {
+      user: User;
       createUser: CreateUserIn;
       createSession: CreateSessionIn;
     }

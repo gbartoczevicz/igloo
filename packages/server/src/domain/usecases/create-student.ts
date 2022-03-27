cdea8eec019613d5bdb2b057b97f51589be1d70d
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-27 17:45:57 -0300

    chore: add usecase

---
+++ packages/server/src/domain/usecases/create-student.ts
@@ -0,0 +1,59 @@
+import { StudentsRepo, UsersRepo } from "~/contracts/database/repositories";
+import { CreateStudentIn } from "~/dtos";
+import { DomainError } from "~/errors";
+import { Student } from "../entities";
+import { IdFactory, StudentFactory } from "../factories";
+
+export class CreateStudentUseCase {
+  private readonly studentsRepo: StudentsRepo;
+
+  private readonly usersRepo: UsersRepo;
+
+  private readonly studentFactory: StudentFactory;
+
+  private readonly idFactory: IdFactory;
+
+  public constructor(
+    studentsRepo: StudentsRepo,
+    usersRepo: UsersRepo,
+    studentFactory: StudentFactory,
+    idFactory: IdFactory,
+  ) {
+    this.studentsRepo = studentsRepo;
+    this.usersRepo = usersRepo;
+    this.studentFactory = studentFactory;
+    this.idFactory = idFactory;
+  }
+
+  public async execute(
+    incoming: CreateStudentIn,
+  ): Promise<Student> {
+    const { manager } = incoming;
+
+    const studentUserId = this.idFactory.create(incoming.studentUserId);
+
+    const alreadyRegistered = await this.studentsRepo
+      .findByInstitutionAndUser(manager.institutionId, studentUserId);
+
+    if (alreadyRegistered) {
+      throw new DomainError(
+        "The student is already created in this institution",
+      );
+    }
+
+    const userExists = await this.usersRepo.findById(studentUserId);
+
+    if (!userExists) {
+      throw new DomainError("User to be student does not exists");
+    }
+
+    const student = this.studentFactory.create({
+      institutionId: manager.institutionId,
+      userId: studentUserId,
+    });
+
+    await this.studentsRepo.save(student);
+
+    return student;
+  }
+}

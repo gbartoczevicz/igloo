5f2e87705b190a9dd4c5407eb4ad22270230025c
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-27 16:49:22 -0300

    chore: add controller

---
+++ packages/server/src/domain/controllers/create-professor.ts
@@ -0,0 +1,24 @@
+import * as P from "~/contracts/presentation";
+import * as D from "~/dtos";
+import { CreateProfessorOut } from "~/dtos";
+import { CreateProfessorUseCase } from "../usecases";
+
+export class CreateProfessorController
+  extends P.Controller<D.CreateProfessorIn, D.CreateProfessorOut> {
+  private readonly usecase: CreateProfessorUseCase;
+
+  public constructor(usecase: CreateProfessorUseCase) {
+    super();
+    this.usecase = usecase;
+  }
+
+  protected override async handle(
+    incoming: D.CreateProfessorIn,
+  ): Promise<P.Result<D.CreateProfessorOut>> {
+    const professorCreated = await this.usecase.execute(incoming);
+
+    const outcoming = new CreateProfessorOut(professorCreated);
+
+    return this.onCreated(outcoming);
+  }
+}
--- packages/server/src/domain/controllers/index.ts
+++ packages/server/src/domain/controllers/index.ts
@@ -3,3 +3,4 @@ export * from "./create-session";
 export * from "./create-institution-and-manager";
 export * from "./authenticate-user";
 export * from "./authenticate-manager";
+export * from "./create-professor";

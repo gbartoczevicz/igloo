d9f088e6b8129ee6928c58ebc6a90fd6f213f9d0
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-31 20:22:58 -0300

    chore: fetch users by id at usecase

--- packages/server/src/domain/usecases/get-professors-by-manager.ts
+++ packages/server/src/domain/usecases/get-professors-by-manager.ts
@@ -1,16 +1,41 @@
-import { ProfessorsRepo } from "~/contracts/database/repositories";
-import { InstitutionManager, Professor } from "../entities";
+import { ProfessorsRepo, UsersRepo } from "~/contracts/database/repositories";
+import { InstitutionManager, Professor, User } from "../entities";
 
+type Result = Map<number, { user: User; professor: Professor }>;
 export class GetProfessorsByManagerUseCase {
   private readonly professorsRepository: ProfessorsRepo;
 
-  public constructor(professorsRepository: ProfessorsRepo) {
+  private readonly usersRepository: UsersRepo;
+
+  public constructor(
+    professorsRepository: ProfessorsRepo,
+    usersRepository: UsersRepo,
+  ) {
     this.professorsRepository = professorsRepository;
+    this.usersRepository = usersRepository;
   }
 
-  public async execute(manager: InstitutionManager): Promise<Professor[]> {
-    return await this.professorsRepository.findAllByInstitution(
+  public async execute(manager: InstitutionManager): Promise<Result> {
+    const professors = await this.professorsRepository.findAllByInstitution(
       manager.institutionId,
     );
+
+    const users = await this.usersRepository.findAllById(
+      professors.map((professor) => professor.id),
+    );
+
+    const result: Result = new Map();
+
+    professors.forEach((professor, position) => {
+      const userFound = users.find((user) => user.id.isEqual(professor.id));
+
+      if (!userFound) {
+        throw new Error(`User not found with professor ${professor.id.value}`);
+      }
+
+      result.set(position, { user: userFound, professor });
+    });
+
+    return result;
   }
 }

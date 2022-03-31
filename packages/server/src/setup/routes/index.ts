902a5e0e809312f19e99dee846f9247727cefdaa
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-31 20:59:56 -0300

    chore: add a new route

--- packages/server/src/setup/routes/index.ts
+++ packages/server/src/setup/routes/index.ts
@@ -3,3 +3,4 @@ export * from "./create-session";
 export * from "./create-institution";
 export * from "./create-professor";
 export * from "./create-student";
+export * from "./get-professors-by-manager";

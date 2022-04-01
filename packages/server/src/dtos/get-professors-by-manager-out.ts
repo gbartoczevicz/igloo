812144678097e40c4e2c39b3fc21e2825db50892
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-03-31 21:08:50 -0300

    fix: dto

--- packages/server/src/dtos/get-professors-by-manager-out.ts
+++ packages/server/src/dtos/get-professors-by-manager-out.ts
@@ -9,11 +9,7 @@ export class GetProfessorsByManagerOut
 
   public toRaw(): unknown {
     return this.outcoming.map((o) => ({
-      professor: {
-        id: o.professor.id.value,
-        userId: o.professor.userId.value,
-        institutionId: o.professor.institutionId.value,
-      },
+      id: o.professor.id.value,
       user: {
         id: o.user.id.value,
         name: o.user.name,

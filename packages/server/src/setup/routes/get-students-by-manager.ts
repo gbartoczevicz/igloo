5f9bca07160b635661253f178e702fb6721870cc
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-04 19:37:14 -0300

    chore: add new route

---
+++ packages/server/src/setup/routes/get-students-by-manager.ts
@@ -0,0 +1,42 @@
+import { HttpMiddleware, HttpRoute } from "~/adapters/http";
+import { Method } from "~/contracts/http";
+import { SystemSetup } from "~/contracts/setup/system";
+import { GetStudentsByManagerController } from "~/domain/controllers";
+import { GetStudentsByManagerUseCase } from "~/domain/usecases";
+import { GetStudentsByManagerIn } from "~/dtos";
+
+export function setupGetStudentsByManager(
+  systemSetup: SystemSetup,
+  userAuthenticated: HttpMiddleware,
+  managerAuthenticated: HttpMiddleware,
+) {
+  const getStudentsByManagerUseCase = new GetStudentsByManagerUseCase(
+    systemSetup.repositories.professorsRepo,
+    systemSetup.repositories.usersRepo,
+  );
+
+  const controller = new GetStudentsByManagerController(
+    getStudentsByManagerUseCase,
+  );
+
+  return new HttpRoute(
+    "/institutions/:institutionId/students",
+    Method.get,
+    (req, res, _next) => {
+      controller.execute(req.getStudentsByManager).then((result) =>
+        res.status(result.status).json(result.content.toRaw())
+      );
+    },
+    [userAuthenticated, managerAuthenticated, (req, res, next) => {
+      const result = GetStudentsByManagerIn.create({ manager: req.manager });
+
+      if (result instanceof GetStudentsByManagerIn) {
+        req.getStudentsByManager = result;
+
+        return next();
+      }
+
+      return res.status(result.status).json(result.content.toRaw());
+    }],
+  );
+}

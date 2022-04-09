fc21e68279628ef1d250eee5d9b2702a6b643bd8
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-09 16:44:34 -0300

    chore: add controller (wip)

---
+++ packages/server/src/domain/controllers/get-managers-by-user.ts
@@ -0,0 +1,23 @@
+import * as P from "~/contracts/presentation";
+import * as D from "~/dtos";
+import { GetManagersByUserUseCase } from "../usecases";
+
+export class GetManagersByUserController
+  extends P.Controller<D.GetManagersByUserIn, D.GetManagersByUserOut> {
+  private readonly usecase: GetManagersByUserUseCase;
+
+  public constructor(usecase: GetManagersByUserUseCase) {
+    super();
+    this.usecase = usecase;
+  }
+
+  protected override async handle(
+    incoming: D.GetManagersByUserIn,
+  ): Promise<P.Result<D.GetManagersByUserOut>> {
+    const managersFound = await this.usecase.execute(incoming.user);
+
+    const outcoming = D.GetManagersByUserOut(managersFound);
+
+    return this.onOk(outcoming);
+  }
+}

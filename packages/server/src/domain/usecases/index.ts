094aac314b455c442f39ca29643d660118331dce
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-09 17:11:59 -0300

    fix: export the usecase

--- packages/server/src/domain/usecases/index.ts
+++ packages/server/src/domain/usecases/index.ts
@@ -10,4 +10,4 @@ export * from "./create-student";
 export * from "./get-professors-by-manager";
 export * from "./get-students-by-manager";
 export * from "./get-users";
-export * from "./get-managers-by-user";
+export * from "./is-user-a-manager";

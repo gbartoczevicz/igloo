0f83e1e2a56949909b0e2bc7c207c9394ee8f085
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-09 17:07:16 -0300

    chore: change usecase impl

--- packages/server/src/domain/usecases/is-user-a-manager.ts
+++ packages/server/src/domain/usecases/is-user-a-manager.ts
@@ -1,14 +1,16 @@
 import { InstitutionManagersRepo } from "~/contracts/database/repositories";
-import { InstitutionManager, User } from "../entities";
+import { User } from "../entities";
 
-export class GetManagersByUserUseCase {
+export class IsUserAManagerUseCase {
   private readonly managersRepo: InstitutionManagersRepo;
 
   public constructor(managersRepo: InstitutionManagersRepo) {
     this.managersRepo = managersRepo;
   }
 
-  public async execute(user: User): Promise<InstitutionManager[]> {
-    return await this.managersRepo.findAllByUserId(user.id);
+  public async execute(user: User): Promise<boolean> {
+    const managersFound = await this.managersRepo.findAllByUserId(user.id);
+
+    return managersFound.length > 0
   }
 }

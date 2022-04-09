0c2f65bffb1400723ef90fdad2a465a54ff927b4
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-09 17:08:32 -0300

    Revert "chore: add controller (wip)"
    This reverts commit fc21e68279628ef1d250eee5d9b2702a6b643bd8.

--- packages/server/src/domain/controllers/index.ts
+++ packages/server/src/domain/controllers/index.ts
@@ -8,4 +8,3 @@ export * from "./create-student";
 export * from "./get-professors-by-manager";
 export * from "./get-students-by-manager";
 export * from "./get-users";
-export * from "./get-managers-by-user";

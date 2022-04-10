8c50ea5edb417ecc01d5f0774794ce35e8ffb754
Matheus de Castro Diori <matheus.diori@sistemafiep.org.br>
2022-04-09 23:50:52 -0300

    fix: remove surname requirement on signup

--- packages/frontend/src/validations/signup-form-validation-schema.js
+++ packages/frontend/src/validations/signup-form-validation-schema.js
@@ -6,8 +6,7 @@ const signup = messages.form.signup;
 export const schema = Yup.object().shape({
   name: Yup.string()
     .required(signup.name_required),
-  surname: Yup.string()
-    .required(signup.surname_required),
+  surname: Yup.string(),
   phone: Yup.string().required(signup.phone_required),
   email: Yup.string()
     .required(signup.email_required)

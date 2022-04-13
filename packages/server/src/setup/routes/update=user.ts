cb96d24b59ed6bfc7dfa66171637295451464208
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-12 23:23:45 -0300

    chore: add new route

---
+++ packages/server/src/setup/routes/update=user.ts
@@ -0,0 +1,42 @@
+import { HttpMiddleware, HttpRoute } from "~/adapters/http";
+import { Method } from "~/contracts/http";
+import { SystemSetup } from "~/contracts/setup/system";
+import { UpdateUserController } from "~/domain/controllers";
+import { UpdateUserUseCase } from "~/domain/usecases";
+import { UpdateUserIn } from "~/dtos";
+
+export function setupUpdateUser(
+  systemSetup: SystemSetup,
+  userAuthenticated: HttpMiddleware,
+) {
+  const usecase = new UpdateUserUseCase(
+    systemSetup.factories.userFactory,
+    systemSetup.repositories.usersRepo
+  );
+
+  const controller = new UpdateUserController(usecase);
+
+  return new HttpRoute(
+    "/users",
+    Method.put,
+    (req, res, _next) => {
+      controller.execute(req.updateUser).then((result) =>
+        res.status(result.status).json(result.content.toRaw())
+      );
+    },
+    [userAuthenticated, (req, res, next) => {
+      const result = UpdateUserIn.create({
+        ...req.body,
+        id: req.authenticatedUserIn.user.id.value,
+      });
+
+      if (result instanceof UpdateUserIn) {
+        req.updateUser = result;
+
+        return next();
+      }
+
+      return res.status(result.status).json(result.content.toRaw());
+    }],
+  );
+}

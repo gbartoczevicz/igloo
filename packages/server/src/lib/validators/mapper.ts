264a54d9c52040bd576d66f9442b49b1738fcdd4
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-17 13:48:17 -0300

    fix: validator mapping

--- packages/server/src/lib/validators/mapper.ts
+++ packages/server/src/lib/validators/mapper.ts
@@ -5,9 +5,21 @@ type ValidatorFunMapping = Record<Options, (v: unknown) => boolean>;
 const isNil = (v: unknown) => v === undefined || v === null;
 
 export const validatorMapping: ValidatorFunMapping = {
-  [Options.optionalList]: (v) => !isNil(v) && Array.isArray(v),
-  [Options.optionalNumber]: (v) => !isNil(v) && !Number.isNaN(v),
-  [Options.optionalString]: (v) => !isNil(v) && typeof v === "string",
+  [Options.optionalList]: (v) => {
+    if (isNil(v)) return true;
+
+    return Array.isArray(v);
+  },
+  [Options.optionalNumber]: (v) => {
+    if (isNil(v)) return true;
+
+    return !Number.isNaN(v);
+  },
+  [Options.optionalString]: (v) => {
+    if (isNil(v)) return true;
+
+    return typeof v === "string";
+  },
   [Options.requiredList]: (v) => Array.isArray(v),
   [Options.requiredNumber]: (v) => !Number.isNaN(v),
   [Options.requiredString]: (v) => typeof v === "string",

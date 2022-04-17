cd0c9dea618c3c011ca877f3cf3585b2e3f4a4b1
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-17 13:29:18 -0300

    chore: use new controller

--- packages/server/src/setup/routes/create-user.ts
+++ packages/server/src/setup/routes/create-user.ts
@@ -1,9 +1,8 @@
 import { HttpRoute } from "~/adapters/http";
 import { Method } from "~/contracts/http";
 import { SystemSetup } from "~/contracts/setup/system";
-import { CreateUserController } from "~/domain/controllers";
+import { CreateUserController } from "~/presentation/create-user-controller";
 import { CreateUserUseCase } from "~/domain/usecases";
-import { CreateUserIn } from "~/dtos";
 
 export function setupCreateUsers(systemSetup: SystemSetup) {
   const usecase = new CreateUserUseCase(
@@ -16,22 +15,9 @@ export function setupCreateUsers(systemSetup: SystemSetup) {
     "/users",
     Method.post,
     (req, res, _next) => {
-      controller.execute(req.createUser).then((result) =>
-        res.status(result.status).json(result.content.toRaw())
+      controller.execute(req.body).then((result) =>
+        res.status(result.status).json(result.content)
       );
-    },
-    (req, res, next) => {
-      const result = CreateUserIn.create(req.body);
-
-      if (result instanceof CreateUserIn) {
-        req.createUser = result;
-
-        return next();
-      }
-
-      res.status(result.status).json(
-        result.content.map((c) => ({ [c.field]: c.message })),
-      );
-    },
+    }
   );
 }

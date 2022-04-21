9492da7108059a1fc816567ad7275ac8a85794eb
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-21 17:31:45 -0300

    chore: update route with the new controller

--- packages/server/src/setup/routes/update-user.ts
+++ packages/server/src/setup/routes/update-user.ts
@@ -1,9 +1,8 @@
 import { HttpMiddleware, HttpRoute } from "~/adapters/http";
 import { Method } from "~/contracts/http";
 import { SystemSetup } from "~/contracts/setup/system";
-import { UpdateUserController } from "~/domain/controllers";
+import { UpdateUserController } from "~/presentation";
 import { UpdateUserUseCase } from "~/domain/usecases";
-import { UpdateUserIn } from "~/dtos";
 
 export function setupUpdateUser(
   systemSetup: SystemSetup,
@@ -21,22 +20,9 @@ export function setupUpdateUser(
     Method.put,
     (req, res, _next) => {
       controller.execute(req.updateUser).then((result) =>
-        res.status(result.status).json(result.content.toRaw())
+        res.status(result.status).json(result.content)
       );
     },
-    [userAuthenticated, (req, res, next) => {
-      const result = UpdateUserIn.create({
-        ...req.body,
-        id: req.authenticatedUserIn.user.id.value,
-      });
-
-      if (result instanceof UpdateUserIn) {
-        req.updateUser = result;
-
-        return next();
-      }
-
-      return res.status(result.status).json(result.content.toRaw());
-    }],
+    [userAuthenticated],
   );
 }

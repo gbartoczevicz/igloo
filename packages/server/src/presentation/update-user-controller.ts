a9bee3f3ad7c31d0efc7fd0a8c6eacc357171765
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-21 17:26:26 -0300

    chore: add new controller

---
+++ packages/server/src/presentation/update-user-controller.ts
@@ -0,0 +1,24 @@
+import { HttpResult } from "~/contracts/presentation";
+import { UpdateUserUseCase } from "~/domain/usecases";
+import { Controller } from "./controller";
+import { UpdateUserDTO } from "./update-user-dto";
+
+export class UpdateUserController extends Controller {
+  private readonly usecase: UpdateUserUseCase;
+
+  public constructor(usecase: UpdateUserUseCase) {
+    super();
+
+    this.usecase = usecase;
+  }
+
+  protected override async handle(incoming: unknown): Promise<HttpResult> {
+    const toUpdateUser = UpdateUserDTO.In.create(incoming);
+
+    const userUpdated = await this.usecase.execute(toUpdateUser);
+
+    const outcoming = UpdateUserDTO.Out.toRaw(userUpdated);
+
+    return this.onOk(outcoming);
+  }
+}

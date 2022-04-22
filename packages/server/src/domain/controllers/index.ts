20c60beb6242a18ad347529b9273434008d86968
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-04-22 12:36:43 -0300

    chore: add GetSelfAuthenticatedUserController and remove old one

--- packages/server/src/domain/controllers/index.ts
+++ packages/server/src/domain/controllers/index.ts
@@ -1,2 +1,1 @@
 export * from "./get-users";
-export * from "./get-self-profile";
---
+++ packages/server/src/presentation/get-self-authenticated-user-controller.ts
@@ -0,0 +1,13 @@
+import { HttpResult } from "~/contracts/presentation";
+import { Controller } from "./controller";
+import { GetSelfAuthenticatedUserDTO } from "./get-self-authenticated-user-dto";
+
+export class GetSelfAuthenticatedUserController extends Controller {
+  protected override async handle(incoming: unknown): Promise<HttpResult> {
+    const incomingUser = GetSelfAuthenticatedUserDTO.In.create(incoming);
+
+    const outcoming = GetSelfAuthenticatedUserDTO.Out.toRaw(incomingUser.user);
+
+    return this.onOk(outcoming);
+  }
+}

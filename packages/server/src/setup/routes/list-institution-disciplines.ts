08b41ed0a43e9d93db71f47ee1799c443054d207
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-05-01 17:07:30 -0300

    chore: add new route

---
+++ packages/server/src/setup/routes/list-institution-disciplines.ts
@@ -0,0 +1,35 @@
+import { HttpMiddleware, HttpRoute } from "~/adapters/http";
+import { Method } from "~/contracts/http";
+import { SystemSetup } from "~/contracts/setup/system";
+import { ListInstitutionDisciplinesUseCase } from "~/domain/usecases";
+import { ListInstitutionDisciplinesController } from "~/presentation";
+
+export function setupListInstitutionDisciplines(
+  systemSetup: SystemSetup,
+  userAuthenticated: HttpMiddleware,
+  managerAuthenticated: HttpMiddleware,
+) {
+  const usecase = new ListInstitutionDisciplinesUseCase(
+    systemSetup.repositories.disciplinesRepo,
+    systemSetup.repositories.coursesRepo,
+    systemSetup.factories.idFactory,
+  );
+
+  const controller = new ListInstitutionDisciplinesController(usecase);
+
+  return new HttpRoute(
+    "/institutions/:institutionId/disciplines",
+    Method.get,
+    (req, res, _next) => {
+      const incoming = {
+        ...(req.body || {}),
+        manager: req.currentManager,
+      };
+
+      controller.execute(incoming).then((result) =>
+        res.status(result.status).json(result.content)
+      );
+    },
+    [userAuthenticated, managerAuthenticated],
+  );
+}

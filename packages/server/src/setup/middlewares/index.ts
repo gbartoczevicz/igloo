842b8ce0ad6a389c88247963b1cf5a276d424580
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-05-01 12:26:45 -0300

    chore: add new middleware

--- packages/server/src/setup/middlewares/index.ts
+++ packages/server/src/setup/middlewares/index.ts
@@ -2,3 +2,4 @@ export * from "./user-authenticated";
 export * from "./common-request";
 export * from "./manager-authenticated";
 export * from "./is-user-a-manager";
+export * from "./is-course-from-institution";

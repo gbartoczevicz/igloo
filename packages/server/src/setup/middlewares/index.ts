67410ed85fd4a482800008428b030842898ff09a
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-05-01 16:23:34 -0300

    Revert "chore: add new middleware"
    This reverts commit 842b8ce0ad6a389c88247963b1cf5a276d424580.

--- packages/server/src/setup/middlewares/index.ts
+++ packages/server/src/setup/middlewares/index.ts
@@ -2,4 +2,3 @@ export * from "./user-authenticated";
 export * from "./common-request";
 export * from "./manager-authenticated";
 export * from "./is-user-a-manager";
-export * from "./is-course-from-institution";

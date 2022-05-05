1e22db13c484d59ec03bc17d4d9e76b86d2d1d78
Matheus de Castro Diori <matheus.diori@sistemafiep.org.br>
2022-05-05 17:59:01 -0300

    feat: initial institution listing

--- packages/frontend/src/components/sidebar/index.js
+++ packages/frontend/src/components/sidebar/index.js
@@ -4,7 +4,7 @@ import SidebarElements from './components/sidebar-elements';
 const Sidebar = () => {
   return (
     <>
-      <div className="top-0 left-0 w-50 bg-neutral-800 p-5 text-white fixed h-full">
+      <div className="top-0 left-0 w-52 bg-neutral-800 p-5 text-white fixed h-full">
         <div className='py-5'> 
           {SidebarHeader()}
         </div>
--- packages/frontend/src/pages/institutions/index.js
+++ packages/frontend/src/pages/institutions/index.js
@@ -1,11 +1,73 @@
-import { Sidebar } from '../../components';
+import { useEffect, useState } from 'react';
+import { Header, Sidebar } from '../../components';
+import api from '../../services/api';
+import * as fireToast from '../../utils/fire-toast';
 
 const Institutions = () => {
+  const [institutions, setInstitutions] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getInstitutions();
+  }, []);
+
+  const getInstitutions = () => {
+    api.get('/institutions')
+      .then(response => {
+        console.log("RESPONSE: ", response)
+        setInstitutions(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        fireToast.success(error);
+      })
+  }
+
+  const createInstitution = () => {
     return (
-      <div>
-        Institutions
-        <Sidebar/>
+      <div className='bg-gray-700 h-36' key={0}>
+        <div id="institution-info" className="p-3">
+          <span>Criar instituição</span>
+        </div>
       </div>
+      )
+  }
+
+  const renderInstitutions = () => {
+    let renderedInstitutions = institutions?.map((inst, index) => {
+      return (       
+        <div className='bg-gray-700 h-36' key={inst.id}>
+          <div id="institution-info" className="p-3">
+            <ul>
+              <li>{inst.name}</li>
+              <li>CNPJ: {inst.cnpj}</li>
+              <li>Telefone: {inst.phone}</li>
+            </ul>
+          </div>
+        </div>
+      )
+    });
+
+    return (
+      <div className='bg-gray-500 p-5 grid grid-cols-4 gap-4'>
+        {createInstitution()}
+        {renderedInstitutions}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header/>
+      <Sidebar/>
+      {
+        !loading && (
+          <div id="page" className='ml-52 mt-10 h-100'>
+            {renderInstitutions()}
+          </div>
+        )
+      }
+    </div>
     );
   }
   

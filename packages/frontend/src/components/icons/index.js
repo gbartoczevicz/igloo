9f0b767f413e3d05e0d75105ea87e51ad04e9297
Matheus de Castro Diori <matheus.diori@sistemafiep.org.br>
2022-05-05 18:02:10 -0300

    fix: rename plural icons to singular

--- packages/frontend/src/components/icons/index.js
+++ packages/frontend/src/components/icons/index.js
@@ -1,8 +1,8 @@
 export { 
-    FaRegBuilding as Institutions,
-    FaBook as Disciplines,
-    FaChalkboard as Courses,
-    FaProjectDiagram as Projects 
+    FaRegBuilding as Institution,
+    FaBook as Discipline,
+    FaChalkboard as Course,
+    FaProjectDiagram as Project 
 } from 'react-icons/fa';
 export { BsHouseDoor as Home } from "react-icons/bs";
 export { HiOutlineLogout as Logout } from "react-icons/hi";

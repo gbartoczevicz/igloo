688907fc588316b722bb59bb4e497149cbcc0995
Matheus de Castro Diori <matheus.diori@sistemafiep.org.br>
2022-05-07 20:44:12 -0300

    fix: token removal

--- packages/frontend/src/components/sidebar/components/sidebar-elements.js
+++ packages/frontend/src/components/sidebar/components/sidebar-elements.js
@@ -2,10 +2,20 @@ import { Link } from "react-router-dom";
 import sidebarElementsList from "./sidebar-elements-list";
 
 const SidebarElements = () => {
+    const removeToken = () => {
+      localStorage.removeItem('@userToken');
+    }
+
     let sidebarElementsLinks = sidebarElementsList.map((el, index) => {
+      let rmToken = false;
+
+      if(el.url === "/") {
+        rmToken = true;
+      }
+      
       return (
         <li className="relative" key={`${index}-${el.url}`}>
-           <Link to={el.url}>
+           <Link to={el.url} onClick={() => rmToken ? removeToken() : null}>
             <span className="flex items-center text-sm py-5 px-6 h-9 overflow-hidden text-ellipsis whitespace-nowrap rounded hover:text-black hover:bg-blue-50 transition duration-300 ease-in-out">
                 {el.icon}
                 <span className="px-2">

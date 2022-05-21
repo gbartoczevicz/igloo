7fea3c9655163e958506d9f58db576c978eb3dbd
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-05-21 11:36:56 -0300

    test: add GetInstitutionCoursesUseCase

---
+++ packages/server/src/domain/usecases/get-institution-courses.test.ts
@@ -0,0 +1,33 @@
+import { systemTestSetup } from "~/setup/system-test";
+import { Course, InstitutionManager } from "../entities";
+import { Id } from "../entities/values";
+import { GetInstitutionCoursesUseCase } from "./get-institution-courses";
+
+function makeSut() {
+  const {
+    repositories: { coursesRepo },
+  } = systemTestSetup();
+
+  const id = new Id("any");
+
+  return {
+    sut: new GetInstitutionCoursesUseCase(coursesRepo),
+    coursesRepo,
+    course: new Course(id, "any", id),
+    manager: new InstitutionManager(id, id, id),
+  };
+}
+
+describe("Get Institution Courses Use Case Tests", () => {
+  it("should get the expected course list", async () => {
+    const { sut, course, coursesRepo, manager } = makeSut();
+
+    jest.spyOn(coursesRepo, "findAllByInstitutionId").mockImplementationOnce(
+      () => Promise.resolve([course]),
+    );
+
+    const result = await sut.execute(manager);
+
+    expect(result).toContainEqual(course);
+  });
+});

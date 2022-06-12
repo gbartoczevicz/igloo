efa9b3ea40ab34d5f04772f68275f5099be2511c
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-06-12 13:02:05 -0300

    chore: change contract from repo and add its primsa implementation

--- packages/server/src/contracts/database/repositories/class-courses-repo.ts
+++ packages/server/src/contracts/database/repositories/class-courses-repo.ts
@@ -2,7 +2,7 @@ import { ClassCourse } from "~/domain/entities";
 import { ClassStartDate, Id } from "~/domain/entities/values";
 
 export interface ClassCoursesRepo {
-  save(classCourse: ClassCourse): Promise<ClassCourse>;
+  save(classCourse: ClassCourse): Promise<void>;
   findByCourseIdAndStart(
     courseId: Id,
     start: ClassStartDate,

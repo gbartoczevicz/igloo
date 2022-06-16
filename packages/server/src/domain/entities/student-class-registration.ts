96e819858d05b9d0b88af72beec3acceac98010b
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-06-16 13:55:18 -0300

    fix: add id into the new entity

--- packages/server/src/domain/entities/student-class-registration.ts
+++ packages/server/src/domain/entities/student-class-registration.ts
@@ -1,11 +1,14 @@
 import { Id } from "./values";
 
 export class StudentClassRegistration {
+  public readonly id: Id;
+
   public readonly studentId: Id;
 
   public readonly classCourseId: Id;
 
-  public constructor(studentId: Id, classCourseId: Id) {
+  public constructor(id: Id, studentId: Id, classCourseId: Id) {
+    this.id = id;
     this.classCourseId = classCourseId;
     this.studentId = studentId;
   }
--- packages/server/src/domain/factories/student-class-registration-factory.ts
+++ packages/server/src/domain/factories/student-class-registration-factory.ts
@@ -2,6 +2,7 @@ import { StudentClassRegistration } from "../entities";
 import { IdFactory } from "./id-factory";
 
 type Params = {
+  id?: string;
   studentId: string;
   classCourseId: string;
 };
@@ -14,9 +15,10 @@ export class StudentClassRegistrationFactory {
   }
 
   public create(params: Params): StudentClassRegistration {
+    const id = this.idFactory.create(params.id);
     const studentId = this.idFactory.create(params.studentId);
     const classCourseId = this.idFactory.create(params.classCourseId);
 
-    return new StudentClassRegistration(studentId, classCourseId);
+    return new StudentClassRegistration(id, studentId, classCourseId);
   }
 }

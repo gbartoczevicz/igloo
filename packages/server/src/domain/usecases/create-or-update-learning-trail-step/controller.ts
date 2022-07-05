bfba1fc140bcf75688124ac3715f9296af0becbd
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-07-05 19:27:59 -0300

    chore: rename some symbols

--- packages/server/src/domain/usecases/create-or-update-learning-trail-step/controller.ts
+++ packages/server/src/domain/usecases/create-or-update-learning-trail-step/controller.ts
@@ -4,20 +4,20 @@ import { CreateOrUpdateLearningTrailStepUseCase } from "./usecase";
 
 export class CreateOrUpdateLearningTrailStepController extends Controller {
   public constructor(
-    private readonly learningTrailFactory: LearningTrailStepFactory,
+    private readonly stepFactory: LearningTrailStepFactory,
     private readonly usecase: CreateOrUpdateLearningTrailStepUseCase,
   ) {
     super();
   }
 
   protected override async handle(incoming?: unknown): Promise<HttpResult> {
-    const learningTrail = this.learningTrailFactory.toEntity(incoming);
+    const step = this.stepFactory.toEntity(incoming);
     const { institutionId } = incoming as any;
 
-    await this.usecase.execute(learningTrail, institutionId);
+    await this.usecase.execute(step, institutionId);
 
     return this.onCreated(
-      this.learningTrailFactory.toPresentation(learningTrail),
+      this.stepFactory.toPresentation(step),
     );
   }
 }

51144a7c608432961b7be5125bd26dfb28146924
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2022-07-07 12:10:52 -0300

    feature: add new mehod on exams repo

--- packages/server/src/contracts/database/repositories/exams-repo.ts
+++ packages/server/src/contracts/database/repositories/exams-repo.ts
@@ -1,5 +1,7 @@
 import { Exam } from "~/domain/entities";
+import { Id } from "~/domain/entities/values";
 
 export interface ExamsRepo {
   save(exam: Exam): Promise<void>;
+  findByLearningTrailStepId(learningTrailStepId: Id): Promise<Exam | null>;
 }
--- packages/server/src/contracts/database/repositories/fakes/exams-repo.ts
+++ packages/server/src/contracts/database/repositories/fakes/exams-repo.ts
@@ -1,8 +1,15 @@
 import { Exam } from "~/domain/entities";
+import { Id } from "~/domain/entities/values";
 import { ExamsRepo } from "../exams-repo";
 
 export class FakeExamsRepo implements ExamsRepo {
   public async save(_exam: Exam): Promise<void> {
     return Promise.resolve();
+  }
+
+  public async findByLearningTrailStepId(
+    _learningTrailStepId: Id,
+  ): Promise<Exam | null> {
+    return Promise.resolve(null);
   }
 }
